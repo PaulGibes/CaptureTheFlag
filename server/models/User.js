8c1c8402c1c8274118f61fb68901ff0eababbff0
Thomas Wray <twray95@gmail.com>
2023-04-18 10:48:00 -0600

    added typeDefs and resolvers

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,10 @@ const bcrypt = require("bcrypt");
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
-  //high scores or other things here.
+  wins: { type: Number },
+  loses: { type: Number },
+  position: { type: String },
+  hasFlag: { type: Boolean },
 });
 
 const User = model("User", userSchema);
