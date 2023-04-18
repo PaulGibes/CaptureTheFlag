7ddc3c8d7674301eefac619d608617b9b2920d72
Thomas Wray <twray95@gmail.com>
2023-04-18 12:02:02 -0600

    added to front end auth, mutations, and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,28 @@
 import { gql } from "@apollo/client";
+
+export const QUERY_USERS = gql`
+  query allUsers {
+    users {
+      _id
+      username
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query oneUser {
+    user {
+      _id
+      username
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+    }
+  }
+`;
