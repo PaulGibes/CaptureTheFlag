6fc0bf484ccf8ebf9891ebac035715ec3fd4f47f
Thomas Wray <twray95@gmail.com>
2023-04-18 13:30:10 -0600

    added form logic to signup and login

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,6 +1,37 @@
+import { useMutation } from "@apollo/client";
 import React from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 
 function Login() {
+  const [formState, setFormState] = useState({ username: "", password: "" });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    //set the form state equal to itself and then add the value to the name of the target(username or password)
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      //Set the value of the
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return <div>Login</div>;
 }
 
