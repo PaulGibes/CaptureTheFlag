c9b315ee67dec87cacf78e979b5a06edb742c404
Thomas Wray <twray95@gmail.com>
2023-04-18 10:56:49 -0600

    graphql now working with typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,24 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        -id: ID
-        username: String
-        password: String
-        wins: Number
-        loses: Number
-        position: String
-        hasFlag: Boolean
-    }
+  type User {
+    _id: ID
+    username: String
+    password: String
+    wins: Int
+    loses: Int
+    position: String
+    hasFlag: Boolean
+  }
 
-    type Query {
-        users: [User]!
-        user(userId: ID!): User
-    }
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+  }
 
-    type Mutation {
-        addUser(username: String!, password: String!): User
-    }
+  type Mutation {
+    addUser(username: String!, password: String!): User
+  }
 `;
 
 module.exports = typeDefs;
