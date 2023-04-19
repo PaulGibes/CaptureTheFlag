f966a73042d9e9b1a24e1fd3fdeaceb8ed6f7d08
paulgibes <paulgibes@gmail.com>
2023-04-19 15:14:10 -0600

    fixed template for modal with carousel

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         poppins: "Poppins",
+        "1/3": "33.3333%",
+        "2/3": "66.6667%",
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
