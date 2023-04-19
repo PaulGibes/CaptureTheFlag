c78390e0f44a78112ef33e6b435e42bb14ca78d5
paulgibes <paulgibes@gmail.com>
2023-04-19 17:21:02 -0600

    created modal with carousel

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../../styles/globals.css";
 import { Modal, Carousel, Button } from 'flowbite-react'
 import { useState } from "react";
+import img1 from "../../assets/images/1.jpg"
 
 function Landing() {
   const [showModal, setShowModal] = useState(false);
@@ -14,62 +15,58 @@ function Landing() {
   };
 
   return (
-    <React.Fragment>
-      <Button onClick={onClick}>
-        How To Play
-      </Button>
-      <Modal
-        show={showModal} onClose={onClose}
-      >
-        <Modal.Header>
-          Terms of Service
-        </Modal.Header>
-        <Modal.Body>
-          <div className="space-y-6">
-
-            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-              <Carousel>
-                <img
-                  src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-                  alt="..."
-                />
-                <img
-                  src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-                  alt="..."
-                />
-                <img
-                  src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-                  alt="..."
-                />
-                <img
-                  src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-                  alt="..."
-                />
-                <img
-                  src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-                  alt="..."
-                />
-              </Carousel>
-            </div>
-
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-            </p>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
+    <>
+      <React.Fragment>
+        <div className="flex justify-center">
           <Button onClick={onClick}>
-            I accept
+            How To Play
           </Button>
-          <Button
-            color="gray"
-            onClick={onClick}
-          >
-            Decline
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </React.Fragment>
+        </div>
+        <Modal
+          show={showModal} onClose={onClose} size="3xl"
+        >
+          <Modal.Header>
+            How To Play
+          </Modal.Header>
+          <Modal.Body>
+            <div className="space-y-6">
+
+              <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+                <Carousel slide={false}>
+                  <img
+                    src={img1}
+                    alt="..."
+                  />
+                  <img
+                    src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                    alt="..."
+                  />
+                  <img
+                    src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                    alt="..."
+                  />
+                  <img
+                    src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                    alt="..."
+                  />
+                  <img
+                    src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                    alt="..."
+                  />
+                </Carousel>
+              </div>
+
+            </div>
+          </Modal.Body>
+          <Modal.Footer className="flex justify-center">
+            <Button onClick={onClose}>
+              I'm ready to play!
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </React.Fragment>
+
+    </>
   );
 }
 
