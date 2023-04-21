c1c2d326405b891c744fd2d35c568849a63de597
paulgibes <paulgibes@gmail.com>
2023-04-21 14:48:01 -0600

    fixed refresh button error on waitingroom

--- client/src/components/CreateGameModal.js
+++ client/src/components/CreateGameModal.js
@@ -134,7 +134,7 @@ const CreateGameModal = ({ setModalOn, setChoice }) => {
                       Cancel
                     </button>
                     <Link
-                      to={"/"}
+                      to={"/waitingroom"}
                       className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                       Play
